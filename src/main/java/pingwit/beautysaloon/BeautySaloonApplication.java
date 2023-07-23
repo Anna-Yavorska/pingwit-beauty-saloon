1cb2013fedf1907c2adff575ca4bae2f6ff76b0d
Anna Yavorska <aniayavorsk@gmail.com>
2023-07-23 19:53:27 +0200

    Initial commit

---
+++ src/main/java/pingwit/beautysaloon/BeautySaloonApplication.java
@@ -0,0 +1,13 @@
+package pingwit.beautysaloon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySaloonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySaloonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pingwit/beautysaloon/BeautySaloonApplicationTests.java
@@ -0,0 +1,13 @@
+package pingwit.beautysaloon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySaloonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
