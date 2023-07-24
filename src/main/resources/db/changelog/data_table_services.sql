dd7a7ba069656684bcdfc7e353413940dabd1862
Anna Yavorska <aniayavorsk@gmail.com>
2023-07-24 20:24:06 +0200

    add db setup

--- src/main/resources/db/changelog/data_table_services.sql
+++ src/main/resources/db/changelog/data_table_services.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.services(
 	 name, client_id, master_id, date, procedures_id, price)
-	VALUES ('Haircut', 81, 1, '2023-08-10', 1, 43.90),
-	('Manicure', 81, 11, '2023-08-10', 11, 20),
-	('Haircut', 101, 1, '2023-10-10', 1, 43.90);
+	VALUES ('Haircut', 1, 1, '2023-08-10', 1, 43.90),
+	('Manicure', 1, 2, '2023-08-10', 2, 20),
+	('Haircut', 1, 1, '2023-10-10', 1, 43.90);
